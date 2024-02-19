b4b911555b5af7cf9db267c8907100213035a306
Felipe Souza <felipesouza036@gmail.com>
2024-02-19 19:18:11 -0300

    config (talwind): Add brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          500: "#02295a",
+          400: "#473dff",
+          300: "#adbeff",
+          200: "#bfe2fd",
+        },
+        attention: "#ed3548",
+        neutral: {
+          500: "#9699ab",
+          400: "#d6d9e6",
+          300: "#f0f6ff",
+          200: "#fafbff",
+        },
+      },
+      backgroundImage: {
+        "mobile-aside": "url(./public/images/bg-sidebar-mobile.svg)",
+        "desktop-aside": "url(./public/images/bg-sidebar-desktop.svg)",
+      },
+    },
   },
   plugins: [],
 };
