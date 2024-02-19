c4e8a07e2c127a9d469184c290ecacaa939d4ad6
Felipe Souza <felipesouza036@gmail.com>
2024-02-19 17:57:38 -0300

    config: Add tailwind css

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,14 @@
+import "../index.css";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   component: () => (
-    <main>
+    <main className="flex bg-red-500">
       <aside className="p-2 flex gap-2">
         <Link to="/" className="[&.active]:font-bold">
           First Step
         </Link>{" "}
       </aside>
-      <hr />
       <Outlet />
     </main>
   ),
