4b6b82390c4a7e72c0519816556066e4ebac0f7e
Felipe Souza <felipesouza036@gmail.com>
2024-02-19 23:41:17 -0300

    fix (SideMenu): Correct link selection css logic

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -7,12 +7,7 @@ const routes = [
   ["SUMMARY", "/summary"],
 ];
 
-interface SideMenuProps {
-  path: string;
-}
-
-const SideMenu = ({ path }: SideMenuProps) => {
-  console.log(path);
+const SideMenu = () => {
   return (
     <aside className="flex gap-2 justify-center pt-6 bg-mobile-aside h-[120px] sm:h-full bg-cover min-h-20 sm:w-[200px] sm:bg-desktop-aside sm:rounded-md">
       <ul className="flex sm:flex-col gap-3 sm:gap-6">
@@ -21,10 +16,10 @@ const SideMenu = ({ path }: SideMenuProps) => {
             <li>
               <Link
                 to={linkPath}
-                className="[&:active]:text-red-500 w-fit h-fit flex gap-1 items-center justify-center group"
+                className="w-fit h-fit flex gap-1 items-center justify-center group"
               >
                 <span
-                  className={`inline-flex items-center font-bold justify-center h-4 w-4 p-4 mr-2  text-white rounded-[50%] border-white border group-hover:bg-primary-300 group-hover:text-primary-500 transition ${path === linkPath ? "text-primary-500 bg-white" : ""}`}
+                  className={`inline-flex items-center font-bold justify-center h-4 w-4 p-4 mr-2  text-white rounded-[50%] border-white border group-hover:bg-primary-300 group-hover:text-primary-500 transition group-[.active]:text-primary-500 group-[.active]:bg-white`}
                 >
                   {index + 1}
                 </span>
