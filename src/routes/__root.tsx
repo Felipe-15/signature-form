065e918a3a3e55bf5bfb9a6d39330249aca31926
Felipe Souza <felipesouza036@gmail.com>
2024-02-20 11:12:33 -0300

    fix (root): Correct responsive background colors

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,8 +4,8 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   component: () => (
-    <main className="flex flex-col sm:flex-row sm:p-4 bg-white shadow-md rounded-md w-full h-screen max-h-[480px] max-w-[800px] m-auto">
-      <SideMenu path={Route.path} />
+    <main className="flex flex-col sm:flex-row sm:p-4 bg-light-300 sm:bg-white shadow-md rounded-md w-full h-screen sm:max-h-[480px] max-w-[800px] m-auto">
+      <SideMenu />
       <Outlet />
     </main>
   ),
