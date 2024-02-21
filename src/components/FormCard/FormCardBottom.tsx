720debddb0eb73beb12a0db6c37304ac4c73f0b0
Felipe Souza <felipesouza036@gmail.com>
2024-02-21 11:25:22 -0300

    feat (FormCard): Add previous link

--- src/components/FormCard/FormCardBottom.tsx
+++ src/components/FormCard/FormCardBottom.tsx
@@ -1,11 +1,23 @@
-// interface FormCardBottomProps {
-//     children: React.ReactNode;
-//   }
+import { Link } from "@tanstack/react-router";
 
-const FormCardBottom = () => {
+interface FormCardBottomProps {
+  previousURL?: string;
+}
+
+const FormCardBottom = ({ previousURL }: FormCardBottomProps) => {
   return (
     <footer className="mt-auto fixed sm:static bottom-0 left-0 right-0 bg-white px-2 py-4 flex justify-center sm:justify-end sm:p-0">
-      <div className="flex sm:max-w-max justify-end max-w-[360px] w-full">
+      <div
+        className={`flex items-center sm:max-w-none max-w-[360px] w-full ${previousURL ? "justify-between" : "justify-end"}`}
+      >
+        {!!previousURL && (
+          <Link
+            to={previousURL}
+            className="font-medium text-light-500 transition hover:text-primary-500 text-center h-fit"
+          >
+            Go back
+          </Link>
+        )}
         <button className="bg-primary-500 text-white font-bold py-2 px-6 rounded-md transition hover:bg-primary-400">
           Next Step
         </button>
