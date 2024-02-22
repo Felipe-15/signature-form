3e024b2dd891e8b218139ae3bbe9810a45de9ace
Felipe Souza <felipesouza036@gmail.com>
2024-02-21 21:42:37 -0300

    refactor: Change data structure

--- src/db/data.ts
+++ src/db/data.ts
@@ -1,42 +1,63 @@
 export const plans = {
-  yearly: [
+  info: [
     {
       id: 1,
       title: "Arcade",
-      price: 90,
       imageURL: "images/icon-arcade.svg",
     },
     {
       id: 2,
       title: "Advanced",
-      price: 120,
       imageURL: "images/icon-advanced.svg",
     },
     {
       id: 3,
       title: "Pro",
-      price: 150,
       imageURL: "images/icon-pro.svg",
     },
   ],
-  monthly: [
+  prices: {
+    monthly: {
+      1: 9,
+      2: 12,
+      3: 15,
+    },
+    yearly: {
+      1: 90,
+      2: 120,
+      3: 150,
+    },
+  },
+};
+
+export const addons = {
+  info: [
     {
-      id: 4,
-      title: "Arcade",
-      price: 9,
-      imageURL: "images/icon-arcade.svg",
+      id: 1,
+      title: "Online service",
+      description: "Access to multiplayer games",
     },
     {
-      id: 5,
-      title: "Advanced",
-      price: 12,
-      imageURL: "images/icon-advanced.svg",
+      id: 2,
+      title: "Larger storage",
+      description: "Extra 1TB cloud save",
     },
     {
-      id: 6,
-      title: "Pro",
-      price: 15,
-      imageURL: "images/icon-pro.svg",
+      id: 3,
+      title: "Customizable profile",
+      description: "Custom theme on your profile",
     },
   ],
+  prices: {
+    monthly: {
+      1: 1,
+      2: 2,
+      3: 2,
+    },
+    yearly: {
+      1: 10,
+      2: 20,
+      3: 20,
+    },
+  },
 };
