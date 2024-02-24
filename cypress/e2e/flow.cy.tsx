aaa85fb4372f895f1387c536c7c556cf4bd947b0
Felipe Souza <felipesouza036@gmail.com>
2024-02-24 14:08:40 -0300

    test: Finish E2E flows test

--- cypress/e2e/flow.cy.tsx
+++ cypress/e2e/flow.cy.tsx
@@ -16,8 +16,28 @@ const setPlan = () =>
       yearlyPrice: 90,
     })
   );
+const setAddons = () =>
+  localStorage.setItem(
+    "addons",
+    JSON.stringify([
+      {
+        id: 1,
+        title: "Online service",
+        description: "Access to multiplayer games",
+        monthlyPrice: 1,
+        yearlyPrice: 10,
+      },
+      {
+        id: 2,
+        title: "Larger storage",
+        description: "Extra 1TB cloud save",
+        monthlyPrice: 2,
+        yearlyPrice: 20,
+      },
+    ])
+  );
 
-describe("App flow", () => {
+describe("Initial step flow without localStorage", () => {
   it("should not be possibly to go to other pages without fill the form", () => {
     cy.visit("/");
     cy.get("aside a").last().click();
@@ -41,6 +61,7 @@ describe("App flow", () => {
   });
   it("should be possible to select a plan and switch between types", () => {
     setUser();
+
     cy.visit("/plans");
     const switcher = cy.get("label");
 
@@ -54,25 +75,51 @@ describe("App flow", () => {
     firstPlan.click();
     cy.get("footer button").should("not.be.disabled").click();
   });
-  it("should be possible to go through addons without select anything", () => {
-    cy.visit("/plans");
+});
+
+describe("Main flow", () => {
+  beforeEach(() => {
     localStorage.setItem("type", "monthly");
     setPlan();
     setUser();
+    setAddons();
+  });
+  it("should be possible to go through addons without select anything", () => {
+    cy.visit("/plans");
+
     cy.get("footer button").click();
     cy.get("footer button").should("not.be.disabled").as("btn");
     cy.get("@btn").click();
     cy.url().should("contain", "/summary");
   });
   it("should be possible to select addons", () => {
-    localStorage.setItem("type", "monthly");
-    setUser();
-    setPlan();
+    localStorage.removeItem("addons");
     cy.visit("/plans");
     cy.get("footer button").click();
     cy.get("li label").each((label) => {
       label.trigger("click");
     });
+
     cy.get("input[type=checkbox]").each((input) => expect(input).to.be.checked);
+  });
+  it("should show all selected infos in summary", () => {
+    cy.visit("/summary");
+    cy.get("div p span")
+      .should("contain", "Arcade")
+      .should("contain", "Monthly");
+
+    cy.get("div div span").should("contain", "$9/mo");
+    cy.get("section div ul li").should("have.length", "2");
+  });
+  it("should shows congratulations when confirm subscription and disable links", () => {
+    cy.visit("/summary");
+
+    cy.get("button").click();
+
+    cy.get("h2").should("contain", "Thank you!");
+
+    cy.get("ul li a").first().click();
+
+    cy.url().should("contain", "/summary");
   });
 });
